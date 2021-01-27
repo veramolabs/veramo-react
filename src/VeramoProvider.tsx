4b4be70d6067681922bd0ac7d7f1ea074084c490
Jason Healy <hello@jasonhealy.io>
2021-01-27 13:46:15 +0000

    build: add linting

--- src/VeramoProvider.tsx
+++ src/VeramoProvider.tsx
@@ -1,38 +1,43 @@
 import React, { useContext, useState, useEffect } from 'react'
 import { TAgent, IPluginMethodMap } from '@veramo/core'
-import { createAgentFromConfig, getStoredAgentConfigs, storeAgentConfigs } from './utils'
+import {
+  createAgentFromConfig,
+  getStoredAgentConfigs,
+  storeAgentConfigs,
+} from './utils'
 import { ISerializedAgentConfig } from './types'
 
 const VeramoReactContext = React.createContext<any>({})
 
 export function VeramoProvider<T extends IPluginMethodMap>(props: {
-  children: any;
+  children: any
   agent: TAgent<T>
 }) {
-
   const [agents, setAgents] = useState<Array<TAgent<T>>>([props.agent])
 
   useEffect(() => {
-    setAgents([props.agent].concat(
-      getStoredAgentConfigs().map(c => createAgentFromConfig<T>(c))
-    ))
+    setAgents(
+      [props.agent].concat(
+        getStoredAgentConfigs().map((c) => createAgentFromConfig<T>(c)),
+      ),
+    )
   }, [props.agent])
 
   const addAgentConfig = (config: ISerializedAgentConfig) => {
     const configs = getStoredAgentConfigs().concat(config)
     storeAgentConfigs(configs)
-    setAgents([props.agent].concat(
-      configs.map(c => createAgentFromConfig<T>(c))
-    ))
+    setAgents(
+      [props.agent].concat(configs.map((c) => createAgentFromConfig<T>(c))),
+    )
   }
 
   const removeAgentConfig = (index: number) => {
     const configs = getStoredAgentConfigs()
     configs.splice(index - 1, 1)
     storeAgentConfigs(configs)
-    setAgents([props.agent].concat(
-      configs.map(c => createAgentFromConfig<T>(c))
-    ))
+    setAgents(
+      [props.agent].concat(configs.map((c) => createAgentFromConfig<T>(c))),
+    )
   }
 
   const getAgentConfig = (index: number): ISerializedAgentConfig => {
@@ -44,27 +49,34 @@ export function VeramoProvider<T extends IPluginMethodMap>(props: {
     const configs = getStoredAgentConfigs()
     configs[index - 1] = config
     storeAgentConfigs(configs)
-    setAgents([props.agent].concat(
-      configs.map(c => createAgentFromConfig<T>(c))
-    ))
+    setAgents(
+      [props.agent].concat(configs.map((c) => createAgentFromConfig<T>(c))),
+    )
   }
 
-  return <VeramoReactContext.Provider value={{
-    agent: props.agent,
-    agents,
-    addAgentConfig,
-    removeAgentConfig,
-    updateAgentConfig,
-    getAgentConfig,
-  }}>
-    {props.children}
-  </VeramoReactContext.Provider>
+  return (
+    <VeramoReactContext.Provider
+      value={{
+        agent: props.agent,
+        agents,
+        addAgentConfig,
+        removeAgentConfig,
+        updateAgentConfig,
+        getAgentConfig,
+      }}
+    >
+      {props.children}
+    </VeramoReactContext.Provider>
+  )
 }
 
-export function useVeramo<T extends IPluginMethodMap, C = Record<string, any>>() {
-  return useContext<{ 
-    agent: TAgent<T> & { context?: C },
-    agents: Array<TAgent<T> & { context?: C }>,
+export function useVeramo<
+  T extends IPluginMethodMap,
+  C = Record<string, any>
+>() {
+  return useContext<{
+    agent: TAgent<T> & { context?: C }
+    agents: Array<TAgent<T> & { context?: C }>
     addAgentConfig: (config: ISerializedAgentConfig) => void
     getAgentConfig: (index: number) => ISerializedAgentConfig
     removeAgentConfig: (index: number) => void
