1f68cca7ff1b13d684c987a3950d5b51f47e51e5
Jason Healy <hello@jasonhealy.io>
2021-02-01 14:42:41 +0000

    chore: clean up lib  (#6)
    * refactor: remove lib folder and ignore
    
    * chore: release new version
    
    * chore: add npm ignore
    
    * chore: release new version
    
    * chore: move ts-jet to dev deps

--- lib/utils.d.ts
+++
@@ -1,8 +0,0 @@
-import { ISerializedAgentConfig } from './types'
-export declare function createAgentFromConfig(
-  config: ISerializedAgentConfig,
-): any
-export declare function getStoredAgentConfigs(): ISerializedAgentConfig[]
-export declare function storeAgentConfigs(
-  configs: ISerializedAgentConfig[],
-): void
