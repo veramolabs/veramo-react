f65bda894b0780f0c9840a99ca2b9041c2090efb
Simonas Karuzas <simonas@not.cat>
2021-02-02 12:20:57 +0200

    chore: fix

--- src/types.ts
+++ src/types.ts
@@ -1,35 +1,9 @@
-<<<<<<< HEAD:src/types.ts
-import {
-  IDataStore,
-  IDIDManager,
-  IKeyManager,
-  IResolver,
-  IMessageHandler,
-  IAgentPluginSchema,
-} from '@veramo/core'
-import { ICredentialIssuer } from '@veramo/credential-w3c'
-import { IDIDComm } from '@veramo/did-comm'
-import { IDataStoreORM } from '@veramo/data-store'
-import { ISelectiveDisclosure } from '@veramo/selective-disclosure'
-
-export type IAgent = IDataStore &
-  IDataStoreORM &
-  ICredentialIssuer &
-  IDIDManager &
-  IDIDComm &
-  IKeyManager &
-  IResolver &
-  IMessageHandler &
-  ISelectiveDisclosure
-
-=======
 import { IDataStore, IDIDManager, IKeyManager, IResolver, IMessageHandler, IAgentPluginSchema } from '@veramo/core';
 import { ICredentialIssuer } from '@veramo/credential-w3c';
 import { IDIDComm } from '@veramo/did-comm';
 import { IDataStoreORM } from '@veramo/data-store';
 import { ISelectiveDisclosure } from '@veramo/selective-disclosure';
 export declare type IAgent = IDataStore & IDataStoreORM & ICredentialIssuer & IDIDManager & IDIDComm & IKeyManager & IResolver & IMessageHandler & ISelectiveDisclosure;
->>>>>>> chore: build:lib/types.d.ts
 export interface IContext extends Record<string, any> {
     id?: string;
     name?: string;
