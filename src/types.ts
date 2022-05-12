ca27e96a274362c989a5ad81ea87c95488c9a4e5
nickreynolds <nicholas.s.reynolds@gmail.com>
2022-05-12 05:23:15 -0400

    fix: update import for IDataStoreORM (#103)

--- src/types.ts
+++ src/types.ts
@@ -1,22 +1,37 @@
-import { IDataStore, IDIDManager, IKeyManager, IResolver, IMessageHandler, IAgentPluginSchema } from '@veramo/core';
-import { ICredentialIssuer } from '@veramo/credential-w3c';
-import { IDIDComm } from '@veramo/did-comm';
-import { IDataStoreORM } from '@veramo/data-store';
-import { ISelectiveDisclosure } from '@veramo/selective-disclosure';
-export declare type IAgent = IDataStore & IDataStoreORM & ICredentialIssuer & IDIDManager & IDIDComm & IKeyManager & IResolver & IMessageHandler & ISelectiveDisclosure;
+import {
+  IDataStore,
+  IDataStoreORM,
+  IDIDManager,
+  IKeyManager,
+  IResolver,
+  IMessageHandler,
+  IAgentPluginSchema,
+} from '@veramo/core'
+import { ICredentialIssuer } from '@veramo/credential-w3c'
+import { IDIDComm } from '@veramo/did-comm'
+import { ISelectiveDisclosure } from '@veramo/selective-disclosure'
+export declare type IAgent = IDataStore &
+  IDataStoreORM &
+  ICredentialIssuer &
+  IDIDManager &
+  IDIDComm &
+  IKeyManager &
+  IResolver &
+  IMessageHandler &
+  ISelectiveDisclosure
 export interface IContext extends Record<string, any> {
-    id?: string;
-    name?: string;
-    picture?: string;
+  id?: string
+  name?: string
+  picture?: string
 }
 
 export interface ISerializedAgentConfig {
-    context: IContext;
-    remoteAgents: Array<{
-        url: string;
-        token?: string;
-        enabledMethods: Array<string>;
-        schema?: IAgentPluginSchema;
-        schemaUrl?: string;
-    }>;
+  context: IContext
+  remoteAgents: Array<{
+    url: string
+    token?: string
+    enabledMethods: Array<string>
+    schema?: IAgentPluginSchema
+    schemaUrl?: string
+  }>
 }
